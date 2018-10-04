75ffa28d811910d160d260a2146bec26343c6b17
JonL13 <JonLamont13@Gmail.com>
2018-10-04 14:49:44 -0400

    fixing take initiative to not empty the list everytime

--- initiative/initiativeList.py
+++ initiative/initiativeList.py
@@ -20,18 +20,21 @@ class InitiativeList:
         info = variable.split(" ")
         if len(info) == 4:
           c = Combatant(info[0], info[1], info[2], info[3])
+          self.initiativeList.append(c)
         elif len(info) == 3:
           c = Combatant(info[0], info[1], info[2])
+          self.initiativeList.append(c)
         elif len(info) == 2:
           c = Combatant(info[0], info[1])
+          self.initiativeList.append(c)
         else:
           print("Terribly sorry sire, I did not understand that")
-        self.initiativeList.append(c)
 
     self.initiativeList.sort(key=lambda c : c.initiative, reverse = True)
     print("Indeed sire, I have assembled thy combatants.")
 
   def printInitiative(self):
+    print(self.initiativeList)
     print('{:<4s} {:<10s} {:<5s} {:<5}'.format("Init", "Combatant", "AC", "Health"))
     for combatant in self.initiativeList:
       try:
