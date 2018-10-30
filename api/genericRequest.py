80b8ff2ea9b68fbb275dc2fe938291575ab4a850
JonL13 <jonlamont13@gmail.com>
2018-10-29 23:44:25 -0400

    genericRequest now works - no error handling though

--- api/genericRequest.py
+++ api/genericRequest.py
@@ -1,8 +1,10 @@
 import requests
 import json
+import pprint
 
 class GenericRequest:
   baseUrl = 'http://dnd5eapi.co/api/'
+  pp = pprint.PrettyPrinter(indent=1)
 
   def command(self, request = None):
     if request == None:
@@ -13,20 +15,32 @@ class GenericRequest:
       print("  features, classes/[class name]/level/[integer 1-20]], races, subraces,")
       print("  equipment, conditions, damage-types, magic-schools, monsters")
     elif request != "":
-      self.getRequest(request)
+      self.parseAndExecuteRequest(request)
 
-  def getRequest(self, request):
+  def parseAndExecuteRequest(self, request):
     if request == None:
       print("Sorry sire, I am not able to handle that")
       return
     parsedRequest = request.split(" ")
-    requestedService = self.baseUrl + parsedRequest[0]
-    # print(requestedService)
-    # try:
-    response = requests.get(requestedService)
-    info = json.loads(response.content.decode('utf-8'))
+    firstUrl = self.baseUrl + parsedRequest[0]
+    info = self.getRequest(firstUrl)
+
     results = info['results']
     for i in range(0, info['count']):
-      print(info['results'][i]['name'] + " " + info['results'][i]['url'])
+      if len(parsedRequest) > 1:
+        found = False
+        desiredItem = ' '.join(map(str,parsedRequest[1:]))
+        if desiredItem == info['results'][i]['name']:
+          print("holy shit it worked.")
+          secondInfo = self.getRequest(info['results'][i]['url'])
+          self.pp.pprint(secondInfo)
+          # print(secondInfo)
+      else:
+        print(info['results'][i]['name'] + " " + info['results'][i]['url'])
     # except:
       # print("I'm sorry sire, I cannot complete the request for: " + requestedService)
+
+  def getRequest(self, url):
+    response = requests.get(url)
+    info = json.loads(response.content.decode('utf-8'))
+    return info
