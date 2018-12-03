fb2c69ebf732b5aebb255d48b4bfaeea71bedcbf
JonL13 <JonLamont13@Gmail.com>
2018-12-03 14:06:10 -0500

    adding todos

--- summonedServant.py
+++ summonedServant.py
@@ -9,6 +9,9 @@ import platform
 
 # TODO ideas:
 # log - accept a flag for naming or appending to a new log file
+# put all summoned servant comments in here - they don't belong elsewhere
+# use if __name == "__main__"
+# color coat output with Gabe's thing
 
 def help(command = None):
   print("I can do the following for you, sire:")
