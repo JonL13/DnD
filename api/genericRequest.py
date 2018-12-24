9d24551c18a474f47fc41938eff8fa6c1879f9cc
JonL13 <jonlamont13@gmail.com>
2018-12-23 22:05:05 -0500

    adding functionality to generic request features

--- api/genericRequest.py
+++ api/genericRequest.py
@@ -1,6 +1,5 @@
-import requests
-import json
-import pprint
+import requests, json, pprint, sys
+from utils.color import Color
 
 class GenericRequest:
   baseUrl = 'http://dnd5eapi.co/api/'
@@ -18,27 +17,38 @@ class GenericRequest:
       self.parseAndExecuteRequest(request)
 
   def parseAndExecuteRequest(self, request):
-    if request == None:
-      print("Sorry sire, I am not able to handle that")
-      return
-    parsedRequest = request.split(" ")
-    firstUrl = self.baseUrl + parsedRequest[0]
-    info = self.getRequest(firstUrl)
+    try:
+      if request == None:
+        print("Sorry sire, I am not able to handle that")
+        return
+      parsedRequest = request.split(" ")
+      firstUrl = self.baseUrl + parsedRequest[0]
+      info = self.getRequest(firstUrl)
 
-    results = info['results']
-    for i in range(0, info['count']):
-      if len(parsedRequest) > 1:
-        found = False
-        desiredItem = ' '.join(map(str,parsedRequest[1:]))
-        if desiredItem == info['results'][i]['name']:
-          secondInfo = self.getRequest(info['results'][i]['url'])
-          self.pp.pprint(secondInfo)
+      if len(parsedRequest) < 2 or parsedRequest[1] == "all":
+        self.printAll(info)
       else:
-        print(info['results'][i]['name'] + " " + info['results'][i]['url'])
-    # except:
-      # print("I'm sorry sire, I cannot complete the request for: " + requestedService)
+        sys.stdout.write(Color.RED)
+        results = info['results']
+        print("test results " + results)
+        for i in range(0, info['count']):
+          if len(parsedRequest) > 1:
+            found = False
+            desiredItem = ' '.join(map(str,parsedRequest[1:]))
+            if str.lower(desiredItem) == str.lower(info['results'][i]['name']):
+              secondInfo = self.getRequest(info['results'][i]['url'])
+              self.pp.pprint(secondInfo)
+          else:
+            print(info['results'][i]['name'] + " " + info['results'][i]['url'])
+    except:
+      print("I'm sorry sire, I cannot complete the request for: " + request)
 
   def getRequest(self, url):
     response = requests.get(url)
     info = json.loads(response.content.decode('utf-8'))
     return info
+
+  def printAll(self, info):
+    for i in range(info['count']):
+      sys.stdout.write(info['results'][i]['name'] + ", ")
+    print()
