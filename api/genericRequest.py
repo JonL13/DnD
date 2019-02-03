2e1e73fdb7f4f5bb1a256d5886c99ee04873d9e4
JonL13 <jonlamont13@gmail.com>
2019-02-03 08:47:20 -0500

    adding optional printout for diceRoller, so it can be used in other functions

--- api/genericRequest.py
+++ api/genericRequest.py
@@ -17,19 +17,19 @@ class GenericRequest:
       print("  features, classes/[class name]/level/[integer 1-20]], races, subraces,")
       print("  equipment, conditions, damage-types, magic-schools, monsters")
     elif request != "all" and request != "help":
-      self.parseAndExecuteRequest(request)
+      self._parseAndExecuteRequest(request)
 
-  def parseAndExecuteRequest(self, request):
+  def _parseAndExecuteRequest(self, request):
     try:
       if request == None:
         print("Sorry sire, I am not able to handle that")
         return
       parsedRequest = request.split(" ")
       firstUrl = self.baseUrl + parsedRequest[0]
-      info = self.getRequest(firstUrl)
+      info = self._getRequest(firstUrl)
 
       if len(parsedRequest) < 2 or parsedRequest[1] == "all":
-        self.printAll(info)
+        self._printAll(info)
       else:
         sys.stdout.write(Color.RED)
         results = info['results']
@@ -38,21 +38,21 @@ class GenericRequest:
             found = False
             desiredItem = ' '.join(map(str,parsedRequest[1:]))
             if str.lower(desiredItem) == str.lower(info['results'][i]['name']):
-              secondInfo = self.getRequest(info['results'][i]['url'])
+              secondInfo = self._getRequest(info['results'][i]['url'])
               self.pp.pprint(secondInfo)
           else:
             print(info['results'][i]['name'] + " " + info['results'][i]['url'])
     except Exception as e:
       print("I'm sorry sire, I cannot complete the request for: " + request)
-      print("I had this issue: " + str(e))
-      traceback.print_exc()
+      # print("I had this issue: " + str(e))
+      # traceback.print_exc()
 
-  def getRequest(self, url):
+  def _getRequest(self, url):
     response = requests.get(url)
     info = json.loads(response.content.decode('utf-8'))
     return info
 
-  def printAll(self, info):
+  def _printAll(self, info):
     for i in range(info['count']):
       sys.stdout.write(ftfy.fix_text(info['results'][i]['name'] + ", "))
     print()
--- utils/diceRoller.py
+++ utils/diceRoller.py
@@ -4,25 +4,28 @@ class DiceRoller:
   def __init__(self):
     self.operators = ["+", "-"]
 
-  def getTotalRollValue(self, parsedCommand):
+  def getTotalRollValue(self, parsedCommand, isOutput = True):
     total = 0
     operation = "+"
     for token in parsedCommand:
       if token in self.operators:
         operation = token
-        sys.stdout.write(" " + operation + " ")
+        if isOutput is True:
+          sys.stdout.write(" " + operation + " ")
       else:
-        value = self.getTokenValue(token)
-        # sys.stdout.write(str(value) + " ")
+        value = self._getTokenValue(token, isOutput)
         if operation == "+":
           total += value
         if operation == "-":
           total -= value
-    print(" = " + str(total))
+    if isOutput is True:
+      print(" = " + str(total))
+    return int(total)
 
-  def getTokenValue(self, token):
-    if self.representsInt(token):
-      sys.stdout.write(str(token))
+  def _getTokenValue(self, token, isOutput):
+    if self._representsInt(token):
+      if isOutput is True:
+        sys.stdout.write(str(token))
       return int(token)
     if 'd' not in token:
       raise ValueError("Invalid input with " + token)
@@ -30,24 +33,27 @@ class DiceRoller:
     if len(roll) == 2:
       if roll[0] == '':
         roll[0] = 1
-      if self.representsInt(roll[0]) and self.representsInt(roll[1]):
+      if self._representsInt(roll[0]) and self._representsInt(roll[1]):
         multiplier = int(roll[0])
         modulus = int(roll[1])
       else:
         raise ValueError("Invalid input with " + token)
     total = 0
-    sys.stdout.write("(")
+    if isOutput is True:
+      sys.stdout.write("(")
     for i in range(multiplier):
       value = random.randint(1,modulus)
-      sys.stdout.write(str(value))
-      if i != multiplier - 1:
+      if isOutput is True:
+        sys.stdout.write(str(value))
+      if i != multiplier - 1 and isOutput is True:
         sys.stdout.write(" + ")
       total += value
-    sys.stdout.write(")")
+    if isOutput is True:
+      sys.stdout.write(")")
     return total
 
 
-  def representsInt(self, s):
+  def _representsInt(self, s):
     try:
       int(s)
       return True
